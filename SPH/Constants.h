b9172b3caa55975e72a773777fed4cda600ab1dd
Jonte <106551147+JCatton@users.noreply.github.com>
2024-02-16 18:00:22 +0000

    Updated Simulation + Constants File

---
+++ SPH/Constants.h
@@ -0,0 +1,19 @@
+//
+// Created by jonte on 16/02/2024.
+//
+
+#ifndef PLANETARY_FORMATION_SIMULATION_CONSTANTS_H
+#define PLANETARY_FORMATION_SIMULATION_CONSTANTS_H
+
+namespace Constants {
+    const float boundsSize[2] {1080, 1920};
+    const float damping {1.f};
+    const int particleNumber {100};
+    const float particleSize {10.f};
+    const float particleSpacing {1.f};
+    const float smoothingRadius {20.f};
+
+}
+
+
+#endif //PLANETARY_FORMATION_SIMULATION_CONSTANTS_H
--- SPH/simulate.cpp
+++ SPH/simulate.cpp
@@ -2,19 +2,12 @@
 #include <array>
 #include <Eigen/Dense>
 #include <cmath>
-//#include <SDL.h>
+#include <fstream>
+#include "Constants.h"
 
 using Eigen::Vector2f;
 
-// Simulation Constants
-
-const Vector2f boundsSize {1080, 1920};
-const float damping {1.f};
-const int particleNumber {100};
-float particleSize {10.f};
-float particleSpacing {1.f};
-
-
+const Vector2f boundsSize {Constants::boundsSize[0], Constants::boundsSize[1]};
 
 
 float sign(float value) {
@@ -26,37 +19,59 @@ void resolveBoundaryCollisions(Vector2f &position, Vector2f &velocity, float& pa
 
     if (std::abs(position.x()) > halfBoundsSize.x()) {
         position.x() = halfBoundsSize.x() * sign(position.x());
-        velocity.x() *= -1 * damping;
+        velocity.x() *= -1 * Constants::damping;
     }
 
     if (std::abs(position.y()) > halfBoundsSize.y()) {
         position.y() = halfBoundsSize.y() * sign(position.y());
-        velocity.y() *= -1 * damping;
+        velocity.y() *= -1 * Constants::damping;
     }
 
+}
+
+void plotter(std::array<Vector2f, Constants::particleNumber> &positions) {
+    std::ofstream MyFile(R"(C:\Users\jonte\CLionProjects\Planetary_Formation_Simulation\Position_Data.txt)");
+    if (MyFile.is_open())
+    {
+        std::cout << "Greetings";
+        for(auto position: positions){
+            std::cout << "X position is: " << position.x() << std::endl;
+            MyFile << position.x() << "," << position.y() << "\n";
+        }
+        MyFile.close();
+    }
+    else std::cout << "Unable to open file";
 }
 
 
 void start() {
-    std::array<Vector2f, particleNumber> positions {};
-    std::array<Vector2f, particleNumber> velocities {};
+    std::array<Vector2f, Constants::particleNumber> positions {};
+    std::array<Vector2f, Constants::particleNumber> velocities {};
 
-    int particlesPerRow = static_cast<int>(std::sqrt(particleNumber));
-    int particlesPerCol = (particleNumber - 1) / particlesPerRow + 1;
-    float spacing = particleSize*2 + particleSpacing;
+    int particlesPerRow = static_cast<int>(std::sqrt(Constants::particleNumber));
+    int particlesPerCol = (Constants::particleNumber - 1) / particlesPerRow + 1;
+    float spacing = Constants::particleSize*2 + Constants::particleSpacing;
 
-    for (int i = 0; i < particleNumber; i++) {
+    for (int i = 0; i < Constants::particleNumber; i++) {
         float x = (i % particlesPerRow - particlesPerRow / 2.f + 0.5f) * spacing;
-        float y = (i / particlesPerRow - particlesPerRow / 2.f + 0.5f) * spacing;
+        float y = (i / particlesPerRow - particlesPerCol / 2.f + 0.5f) * spacing;
         positions[i] = {x, y};
     }
+
+    plotter(positions);
 }
 
-void update(float deltaT) {
-    for (int i {0}; i< std::size(positions); i++) {
-        positions[i] = positions[i] + velocities[i] * deltaT;
-
-        resolveBoundaryCollisions(positions[i], velocities[i], particleSize);
-    }
+int main(int argc, char** argv) {
+    std::cout << "Started";
+    start();
+    std::cout << "Ended...";
 }
+
+//void update(float deltaT) {
+//    for (int i {0}; i< std::size(positions); i++) {
+//        positions[i] = positions[i] + velocities[i] * deltaT;
+//
+//        resolveBoundaryCollisions(positions[i], velocities[i], particleSize);
+//    }
+//}
 
