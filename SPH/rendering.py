9a6e2e324279fa54fb4b282fbbfdfdb71362e1f6
kaimul19 <kaimulcock19@gmail.com>
2024-03-16 04:33:07 +0000

    Corrected a line in Rendering .py And added test.txt to allow folder to push Have adjusted some poor errors on my behalf when first writing the position data function, mainly in writing stuff in the non-obvious way. i.e. I had gridsize be centred about origin so for 1920 you'd have to put 960, also only allowed for square plotting. This is done with xSize and ySize inputs (allowing lower and upper bounds).
    Still runs fairly slow. Will look at better implementing chunk loading tomorrow/today, best use from testing seems to be scaling down axis as for the same number of 'particles', a larger gridsize loads slower.
    
    Also, The issues I mentioned above are still present in the animation class, I will fix these tomorrow as now I am become sleep, the champion of slumber.

--- SPH/rendering.py
+++ SPH/rendering.py
@@ -6,68 +6,55 @@ import matplotlib.pyplot as plt
 import imageio.v2 as imageio
 import os
 import glob
+# Define the smoothing kernel function
 
 
-class PositionData:
-    def __init__(self, data, data_location=None, delimiters=None):
-        if type(data) is str:
-            self.positions = np.loadtxt(data_location, delimiter=delimiters)
-        else:
-            self.positions = data
-        self.gridx = None
-        self.gridy = None
-
-    def display_positions_on_grid(self):
-        plt.plot(self.positions[:, 0], self.positions[:, 1], ".", markersize=3)
-        plt.show()
-
-    def _create_grid(self, grid_size):
-        x = np.linspace(-grid_size[0], grid_size[0], 2 * grid_size[0])
-        y = np.linspace(-grid_size[1], grid_size[1], 2 * grid_size[1])
-        x_grid, y_grid = np.meshgrid(x, y)
-        self.gridx = x_grid
-        self.gridy = y_grid
-
-    def calculate_densities(self, smoothing_radius, grid_size):
-        self._create_grid(grid_size)
-        points = np.stack([self.gridx.ravel(), self.gridy.ravel()], axis=-1)  # Shape: (num_points, 2)
-        # Expand dimensions for broadcasting
-        sample_points_exp = np.expand_dims(points, axis=1)  # Shape: (num_points, 1, 2)
-        positions_exp = np.expand_dims(self.positions, axis=0)  # Shape: (1, num_positions, 2)
-
-        # Calculate squared distances using broadcasting
-        sq_distances = np.sum((sample_points_exp - positions_exp) ** 2, axis=2)  # Shape: (num_points, num_positions)
-
-        # Calculate influence using the smoothing kernel
-        influences = _smoothing_kernel(smoothing_radius, np.sqrt(sq_distances))  # Apply sqrt to get distances
-
-        # Sum up the influences to get densities
-        densities = np.sum(influences, axis=1)  # Shape: (num_points,)
-
-        density_grid = densities.reshape(self.gridx.shape)
-        return density_grid
-
-    def display_densities_heatmap(self, smoothing_radius, grid_size):
-        density_grid = self.calculate_densities(smoothing_radius, grid_size)
-        plt.figure(figsize=(8, 6))
-        plt.contourf(self.gridx, self.gridy, density_grid, cmap='viridis')
-        plt.colorbar()
-        plt.title('Density Distribution')
-        plt.xlabel('X axis')
-        plt.ylabel('Y axis')
-        plt.show()
-
-    def add_noise_to_positions(self, number=50):
-        random_particles = np.random.uniform(low=np.min(self.gridx), high=np.max(self.gridx), size=(number, 2))
-        self.positions = np.vstack((self.positions, random_particles))
-
-
-def _smoothing_kernel(radius, dst):
+def smoothing_kernel(radius, dst):
     volume = np.pi * (radius ** 8) / 4
     value = np.maximum(0, radius ** 2 - dst ** 2)
     return value ** 3 / volume
 
 
+class PositionData:
+    def __init__(self, data_location=None, delimiters=None):
+        if data_location and os.path.exists(data_location):
+            self.positions = np.loadtxt(data_location, delimiter=delimiters)
+        else:
+            self.positions = None
+        self.grid = None
+
+    def display_positions_on_grid(self):
+        plt.plot(self.positions[:, 0], self.positions[:, 1], ".", markersize=3)
+        plt.show()
+
+    def _create_grid(self, x_size: tuple[int, int], y_size: tuple[int, int]):
+        x = np.linspace(x_size[0], x_size[1], x_size[1] - x_size[0] + 1)
+        y = np.linspace(y_size[0], y_size[1], y_size[1] - y_size[0] + 1)
+        self.grid = np.meshgrid(x, y)
+
+    def calculate_densities(self, smoothing_radius, x_size: tuple[int, int], y_size: tuple[int, int]):
+        if self.positions is None:
+            raise ValueError("Position data not loaded.")
+        self._create_grid(x_size, y_size)
+        points = np.stack([self.grid[0].ravel(), self.grid[1].ravel()], axis=-1)
+        sq_distances = np.sum((points[:, np.newaxis, :] - self.positions[np.newaxis, :, :]) ** 2, axis=2)
+        influences = smoothing_kernel(smoothing_radius, sq_distances)
+        densities = np.sum(influences, axis=1)
+        return densities.reshape(self.grid[0].shape)
+
+    def display_densities_heatmap(self, smoothing_radius, x_size: tuple[int, int], y_size: tuple[int, int]):
+        density_grid = self.calculate_densities(smoothing_radius, x_size, y_size)
+        plt.figure(figsize=(10, 8))
+        plt.contourf(self.grid[0], self.grid[1], density_grid, levels=40)
+        plt.colorbar()
+        plt.title('Density Distribution')
+        plt.xlabel('X axis')
+        plt.ylabel('Y axis')
+        plt.tight_layout()
+        plt.show()
+
+
+# Will fix grids on this tomorrow ≈ 13:00/14:00 ish
 class Animation:
     def __init__(self, data_location: str, smoothing_radius, grid_size, delimiters=","):
         self.data_location = data_location
@@ -112,10 +99,11 @@ class Animation:
         for frame_filename in frames:
             os.remove(frame_filename)
 
-test = Animation("Data/AnimationTestData.txt", smoothing_radius=30, grid_size=(60, 60), delimiters=";")
-test.calculate_densities()
-test.animate_densities()
+# test = Animation("Data/AnimationTestData.txt", smoothing_radius=30, grid_size=(1000, 120), delimiters=";")
+# test.calculate_densities()
+# test.animate_densities()
 
+# Old Jonte Stuffs
         # print("Animation saved as density_animation.gif")
 
 # #from particles import Particle
